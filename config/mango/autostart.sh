a9408165cae99d42a67cfb6b793c5b8b3dfb9ddb
Archsplatts <bloodsplatts@gmail.com>
2026-08-27 06:12:49 +0200

    Update autostart.sh

--- config/mango/autostart.sh
+++ config/mango/autostart.sh
@@ -2,19 +2,19 @@
 
 set +e
 
-mako >/dev/null 2>&1 &
-
 gammastep >/dev/null 2>&1 &
-
-swaybg -i ~/Images/Wallpapers/sunken-tower.png >/dev/null 2>&1 &
 
 waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css >/dev/null 2>&1 &
 
-exec dbus-run-session -- pipewire &
-exec dbus-run-session -- pipewire-pulse &
-exec dbus-run-session -- wireplumber &
+swaybg -i ~/Images/Wallpapers/sunken-tower.png >/dev/null 2>&1 &
+
+mako >/dev/null 2>&1 &
+
+pipewire &
+wireplumber &
+pipewire-pulse &
+
+/usr/libexec/xfce-polkit 2>&1 &
 
 wl-clip-persist --clipboard regular --reconnect-tries 0 &
 wl-paste --type text --watch cliphist store -max-items 100 &
-
-/usr/libexec/xfce-polkit 2>&1 &
