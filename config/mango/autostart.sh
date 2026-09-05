1673a270956bac263cccf805897650905dcf5897
Archsplatts <bloodsplatts@gmail.com>
2026-09-05 14:23:17 +0200

    Update autostart.sh

--- config/mango/autostart.sh
+++ config/mango/autostart.sh
@@ -2,19 +2,19 @@
 
 set +e
 
-gammastep >/dev/null 2>&1 &
-
-waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css >/dev/null 2>&1 &
-
-swaybg -i ~/Images/Wallpapers/sunken-tower.png >/dev/null 2>&1 &
-
-mako >/dev/null 2>&1 &
-
 pipewire &
 wireplumber &
 pipewire-pulse &
 
+swaybg -i ~/Images/Wallpapers/nature-valley-1.jpg >/dev/null 2>&1 &
+
+mako >/dev/null 2>&1 &
+
+waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css >/dev/null 2>&1 &
+
 /usr/libexec/xfce-polkit 2>&1 &
+
+gammastep >/dev/null 2>&1 &
 
 wl-clip-persist --clipboard regular --reconnect-tries 0 &
 wl-paste --type text --watch cliphist store -max-items 100 &
